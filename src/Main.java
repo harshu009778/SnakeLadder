662f979dd3198e6c4d93746d0d664b7288d35ec4
harshu009778 <harshu.19791@gmail.com>
2025-12-11 15:05:54 +0530

    Added : random no generated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main{
     public static void main(String[] args) {
+        // ----- uc1 --------
         Player p1 = new Player();
         System.out.println(p1.position);
+
+
+        // ----- UC2 ----
+        p1.generateNumber();
 
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,12 @@
 public class Player {
     int position = 0;
+    int number ;
+
+
+    // ---- uc2 ----
+    public void generateNumber(){
+        number = (int) ((Math.random()*100) % 6) + 1;
+        System.out.println("Randomly generated Number : "+ number);
+    }
 
 }
